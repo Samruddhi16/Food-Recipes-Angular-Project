29f444e161d3667d04dca70688ec122c21313089
Samruddhi16 <samruddhibhapkar61@gmail.com>
2021-07-25 16:07:27 +0530

    5 files modified

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,33 @@
-import { EventEmitter } from '@angular/core';
-import { Recipe } from "./recipe.model";
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable()
 
 export class RecipeService {
-  recipeSelected=new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a Test',
-      'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg'
+      'Taasty Schnitzel',
+      'A Super Taasty Schnitzel-Just Awesome!',
+      'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is simply a Test',
-      'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg'
+      'Big Fat Burger',
+      'What Else you need to say?',
+      'https://assets.cntraveller.in/photos/60ba26c0bfe773a828a47146/master/pass/Burgers-Mumbai-Delivery.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     )
   ];
-
-  getRecipes(){
+  constructor(private slService:ShoppingListService){}
+  getRecipes() {
     return this.recipes.slice();
   }
+
+  addIngredientsToShoppingList(ingredients:Ingredient[]) {
+    this.slService.addIngredient(ingredients);
+    }
 }
