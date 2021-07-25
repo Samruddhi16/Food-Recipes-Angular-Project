1ee9b30e355dd33452a5c5b96e5485a3a26b231c
Samruddhi16 <samruddhibhapkar61@gmail.com>
2021-07-25 13:50:14 +0530

    2 files modified, 2 added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,21 +10,13 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected=new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a Test',
-      'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg'
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is simply a Test',
-      'https://www.simplyrecipes.com/thmb/IbY_MK3pA1G_ZHzWltgZCxe_OUk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-vertical-a-1600-0eafb4cd27b74617abb36379751eed51.jpg'
-    )
-  ];
-  constructor() {}
+  recipes: Recipe[];
 
-  ngOnInit() {}
+  constructor(private recipeService:RecipeService) {}
+
+  ngOnInit() {
+    this.recipes=this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe:Recipe){
     this.recipeWasSelected.emit(recipe);
